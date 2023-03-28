5b6abca4e4b9350ae51354e9ccdeb2c44d6120a2
Darien Schettler <darien_schettler@hotmail.com>
2023-03-28 13:45:55 -0400

    add optional utils

---
+++ cllm_data_curation/parallel_dl/other_utils.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import pickle
+import json
+import os
+
+def load_jsonl(path):
+    """
+    Load a jsonl file into a pandas dataframe
+    """
+    df = pd.read_json(path, lines=True,)
+    return df
+
+
+def save_pickle(obj: object, file_path: str) -> None:
+    """
+    Save an object to a pickle file.
+
+    Args:
+        obj: The object to save.
+        file_path: The path to the pickle file.
+    """
+    with open(file_path, 'wb') as f:
+        pickle.dump(obj, f)
+
+
+def load_pickle(file_path: str) -> object:
+    """
+    Load an object from a pickle file.
+
+    Args:
+        file_path: The path to the pickle file.
+
+    Returns:
+        The loaded object.
+    """
+    with open(file_path, 'rb') as f:
+        obj = pickle.load(f)
+    return obj
